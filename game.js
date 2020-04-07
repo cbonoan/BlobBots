5bce49c73763614b81cec6c58cd5618934a55e4e
Charles Bonoan <charlesbonoan@gmail.com>
2020-04-07 10:34:36 -0700

    created new branch and getting started on game logic

---
+++ game.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+    var config = {
+        width: window.innerWidth, 
+        height: window.innerHeight, 
+        backgroundColor: 0x88fbff
+    }
+
+    var game = new Phaser.Game(config);
+}
