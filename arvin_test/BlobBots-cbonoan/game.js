6f299656e6def8df9f9cf88f47316ca34e1504d3
ashertukde <60154021+ashertukde@users.noreply.github.com>
2020-04-20 08:38:19 -0700

    demo version of lvl1

---
+++ arvin_test/BlobBots-cbonoan/game.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+
+    var config = {
+        width: 1968, 
+        height: 1184, 
+        backgroundColor: 0x000000,
+        scene: [Start, Lvl1]
+    }
+
+    var game = new Phaser.Game(config);
+}
